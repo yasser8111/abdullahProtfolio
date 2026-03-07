c4a775f680f9e55fd04c47a649bd767e8c017cac
yasser8111 <yasserxd653@gmail.com>
2026-03-07 10:35:46 +0300

    make About page responsiv

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,23 +8,24 @@ const About = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto px-8 pt-32 pb-16 min-h-screen">
+      <div className="container min-h-[calc(100dvh-72px)] mx-auto p-4 mt-18 md:p-6 lg:p-8 flex flex-col justify-between">
         <Link
           to="/"
-          className="inline-flex items-center gap-2 text-2xl font-medium text-black-600 hover:text-black mb-10 transition-colors snap"
+          className="inline-flex items-center gap-2 text-xl lg:text-2xl font-medium text-black-600 hover:text-black mb-4 lg:mb-10 transition-colors snap"
         >
-          <i className="fa-solid fa-arrow-left "></i>
+          <i className="fa-solid fa-arrow-left"></i>
           Back to Home
         </Link>
-        <h2 className="text-[52px] font-medium text-black-900 mb-16">
+        
+        <h2 className="text-4xl lg:text-5xl font-medium text-black-600 mb-3 lg:mb-6">
           {siteData.aboutPage.heading}
         </h2>
 
-        <div className="max-w-5xl mb-24 m-auto">
-          <h3 className="text-[44px] font-medium text-black-900 mb-8 tracking-tight">
+        <div className="max-w-5xl mb-12 lg:mb-24 m-auto">
+          <h3 className="text-3xl md:text-4xl lg:text-5xl font-medium text-black-900 mb-4 lg:mb-8">
             {siteData.aboutPage.greeting}
           </h3>
-          <div className="flex flex-col gap-6 text-[22px] leading-[1.6] text-black-600 font-normal ">
+          <div className="flex flex-col gap-3 lg:gap-6 text-xl lg:text-2xl text-black-600 font-normal">
             {siteData.aboutPage.paragraphs.map((text, idx) => (
               <p key={idx}>{text}</p>
             ))}
@@ -32,14 +33,14 @@ const About = () => {
         </div>
 
         <div>
-          <h2 className="text-[52px] font-medium text-black-900 mb-16 snap">
+          <h2 className="text-4xl lg:text-5xl font-medium text-black-600 mb-3 lg:mb-6 snap">
             {siteData.aboutPage.skillsHeading}
           </h2>
 
-          <div className="flex flex-col gap-16 md:gap-20">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-30">
+          <div className="flex flex-col gap-4 sm:gap-16 md:gap-20">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-16 md:gap-20">
               <div>
-                <h3 className="text-4xl text-black-800 mb-6 font-normal">
+                <h3 className="text-3xl md:text-4xl lg:text-5xl text-black-900 mb-4 lg:mb-8">
                   {siteData.aboutPage.development.title}
                 </h3>
                 <div className="flex flex-wrap gap-3">
@@ -50,7 +51,7 @@ const About = () => {
               </div>
 
               <div>
-                <h3 className="text-4xl text-black-800 mb-6 font-normal">
+                <h3 className="text-3xl md:text-4xl lg:text-5xl text-black-900 mb-4 lg:mb-8">
                   {siteData.aboutPage.design.title}
                 </h3>
                 <div className="flex flex-wrap gap-3">
@@ -62,7 +63,7 @@ const About = () => {
             </div>
 
             <div>
-              <h3 className="text-4xl text-black-800 mb-6 font-normal">
+              <h3 className="text-3xl md:text-4xl lg:text-5xl text-black-900 mb-4 lg:mb-8">
                 {siteData.aboutPage.ai.title}
               </h3>
               <div className="flex flex-wrap gap-3">
@@ -74,8 +75,8 @@ const About = () => {
           </div>
         </div>
 
-        <div className="mt-24">
-          <h2 className="text-[52px] font-medium text-black-900 mb-16 snap">
+        <div className="mt-12 lg:mt-24">
+          <h2 className="text-4xl lg:text-5xl font-medium text-black-600 mb-3 lg:mb-6 snap">
             {siteData.aboutPage.certificates.title}
           </h2>
 
@@ -83,27 +84,9 @@ const About = () => {
             {siteData.aboutPage.certificates.list.map((cert, index) => (
               <div
                 key={index}
-                className="group p-8 rounded-2xl bg-black-100 hover:bg-black-200 transition-all"
+                className="group p-8 rounded-2xl bg-black-100 hover:bg-black-200 transition-all h-[35vh]"
               >
-                <div className="flex justify-between items-start mb-6">
-                  <span className="text-black-500 font-medium text-xl">
-                    {cert.year}
-                  </span>
-                  <a
-                    href={cert.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="w-10 h-10 rounded-full bg-white flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"
-                  >
-                    <i className="fa-solid fa-arrow-up-right-from-square text-black-400"></i>
-                  </a>
-                </div>
-                <h4 className="text-2xl font-medium text-black-900 mb-2">
-                  {cert.name}
-                </h4>
-                <p className="text-xl text-black-600 font-normal">
-                  {cert.issuer}
-                </p>
+                
               </div>
             ))}
           </div>
